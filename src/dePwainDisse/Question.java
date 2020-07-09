ff1a53db5b94a9e00c73114b998486ecfd06173a
Ronan-1337 <67144266+Ronan-1337@users.noreply.github.com>
2020-07-09 10:39:01 +0200

    2.10 et 2.11 faits

---
+++ src/dePwainDisse/Question.java
@@ -0,0 +1,29 @@
+package dePwainDisse;
+
+public class Question {
+		String enonce;
+		int difficulte=50; // la difficulte varie de 0 à 100
+		String[] reponse = new String[3];
+		String verite;
+
+		public Question(String enonce, String verite, String reponse1, String reponse2, String reponse3) {
+			super();
+			this.enonce = enonce;
+			this.reponse[0] = reponse1;
+			this.reponse[1] = reponse2;
+			this.reponse[2] = reponse3;
+			this.verite = verite;
+		}
+
+		void setDifficulte(int d){
+		this.difficulte = d;
+		}
+		
+		int getDifficulte(){
+		return this.difficulte;
+		}
+		
+		String getEnonce(){
+		return this.enonce;
+		}
+}
